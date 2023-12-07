9935d7cf3a8aba41fbdc851e1a1b4860f7410e6e
Parralol <110953563+Parralol@users.noreply.github.com>
2023-12-07 13:09:28 -0500

    subir

--- src/main/java/CVDS/Dina/proyecto/controller/login.java
+++ src/main/java/CVDS/Dina/proyecto/controller/login.java
@@ -24,7 +24,7 @@ public class login {
         return "login";
     }
 
-    //prueba
+    //prueba 2
     @PostMapping("/login")
     public String login(String username, String password, Model model) {
          boolean usern = false;
